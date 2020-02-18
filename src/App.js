8775a6e08e7610729eca6c7356008e848a3d1b2a
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-19 00:28:51 +0300

    Header Build

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import Header from './components/Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <Header />
+      </div>
+    )
+  }
 }
 
 export default App;
