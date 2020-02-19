b5dd146309e1b0b2f133745018cd0b0db53f2eaa
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-20 00:49:38 +0300

    Refactored

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -3,63 +3,44 @@ import queryString from 'query-string';
 
 class PlayList extends Component {
 
-    componentDidMount(){
-        let parsed = queryString.parse(window.location.search);
-        let accessToken = parsed.access_token;
-        if(!accessToken)
-            return;
-        fetch('https://api.spotify.com/v1/me', {
-            headers: {'Authorization': 'Bearer' + accessToken }
-        }).then(data => this.setState({
-            user: {
-                name: data.display_name
-            }
-        }))
-        fetch('https://api.spotify.com/v1/me/playlists', {
-            headers: {'Authorization':'Bearer' + accessToken }
-        }).then(response => response.json())
-        .then(playlistData => {
-            let playlists = playlistData.items
-            let trackDataPromises = playlists.map(playlist => {
-                let responsePromise = fetch(playlist.tracks.href, {
-                    headers: {'Authorization':'Bearer' + accessToken }
-                })
-                let trackDataPromises = responsePromise
-                    .then(response => response.json())
-                return trackDataPromise
-            })
-            let allTracksDataPromises = 
-                Promise.all(trackDataPromises)
-            let playlistsPromise = allTracksDataPromises.then(trackDatas => {
-                trackDatas.forEach((trackData, i) => {
-                    playlists[i].trackDatas = trackData.items
-                        .map(item => item.track)
-                        .map(trackData => ({
-                            name:trackData.name,
-                            durations:trackData.duration_ms / 1000
-                        }))
-                })
-                return playlists
-            })
-            return playlistsPromise
-        })
-        .then(playlists => this.setState({
-            playlists: playlists.map(item => {
-                return {
-                    name:item.name,
-                    imageUrl:item.images[0].url,
-                    songs:item.trackDatas.slice(0,3)
-                }
-            })
-        }))
-    }
+    /*componentDidMount(){
+      let parsed = queryString.parse(window.location.search);
+      let accessToken = parsed.access_token;
+      if (!accessToken)
+        return;
+      fetch('https://api.spotify.com/v1/me', {
+        headers: {'Authorization': 'Bearer ' + accessToken}
+      }).then(response => response.json())
+      .then(data => this.setState({
+        user: {
+          name: data.display_name
+        }
+      }))
+  
+      fetch('https://api.spotify.com/v1/me/playlists', {
+        headers: {'Authorization': 'Bearer ' + accessToken}
+      }).then(response => response.json())
+      .then(data => this.setState({
+        playlists: data.items.map(item => {
+          console.log(data.items)
+          return {
+            name: item.name,
+            imageUrl: item.images[0].url, 
+            songs: []
+          }
+      })
+      }))
+  
+    }*/
     render() {
-        return ( 
-            <div class="PlayList-colon col-md-12">
-                <h1>Playlist</h1>
-            </div>
-        )
+      return ( 
+          <div class="PlayList-colon col-md-12">
+              <h1>Playlist</h1>
+          </div>
+      );
     }
-}
+  }
+    
+
 
 export default PlayList;
